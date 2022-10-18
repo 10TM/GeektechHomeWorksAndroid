b44fd7a822b5e5e2b84601e906442e9c90e05c7c
X0K <99002104+10TM@users.noreply.github.com>
2022-10-18 23:41:31 +0600

    Update
    Add calculator

---
+++ Calculator/app/src/main/java/kg/nazar/calculator/SecondActivity.java
@@ -0,0 +1,11 @@
+package kg.nazar.calculator;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
